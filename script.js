4dc27ea608c60df694a9e13324756ce4cb00b8bf
Leftin <prozorovstepan054@gmail.com>
2022-02-16 16:53:18 +0500

    Created site

---
+++ script.js
@@ -0,0 +1,7 @@
+var enter = function() {
+	if(document.getElementById("text").value == "NZTK") {
+		alert("You solved first part of this quest\n\nWait for updates")
+	} else {
+		alert("Wrong answer")
+	}
+}
